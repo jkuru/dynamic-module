291300fb5b552b3f4e4a71d964b7b75fc06a78ae
Jerry Kuru <jerrykuru@gmail.com>
2025-05-07 11:09:36 -0700

    refactored to reduce indirection

--- app/src/main/java/com/kuru/nextgen/NextGenApplication.kt
+++ app/src/main/java/com/kuru/nextgen/NextGenApplication.kt
@@ -1,26 +1,10 @@
 package com.kuru.nextgen
 
 import android.app.Application
-import android.util.Log
-import androidx.datastore.core.DataStore
-import com.kuru.featureflow.component.googleplay.DFComponentInstaller
 import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
-import androidx.datastore.preferences.core.Preferences
-import javax.inject.Inject
 
 @HiltAndroidApp
 class NextGenApplication : Application() {
-
-//    @Inject
-//    lateinit var dataStore: DataStore<Preferences> // From AppModule
-//
-//    @Inject
-//    lateinit var componentInstaller: DFComponentInstaller // From FrameworkBindingsModule
-//
-//    @Inject
-//    lateinit var applicationScope: CoroutineScope // From AppModule
 
     override fun onCreate() {
         super.onCreate()
@@ -28,6 +12,5 @@ class NextGenApplication : Application() {
 
     companion object {
         const val PLANTS_MODULE = "feature-plants"
-        const val TAG = "DynamicFeatureManager"
     }
 }
--- feature_plants/src/main/java/com/kuru/nextgen/plants/PlantsEntry.kt
+++ feature_plants/src/main/java/com/kuru/nextgen/plants/PlantsEntry.kt
@@ -2,62 +2,60 @@ package com.kuru.nextgen.plants
 
 import android.content.Context
 import android.util.Log
-import com.kuru.featureflow.component.register.DFComponentConfig
-import com.kuru.featureflow.component.register.DFComponentEntry
-import com.kuru.featureflow.component.register.DFComponentInterceptor
-import com.kuru.featureflow.component.register.DFComponentRegistry
+import com.kuru.featureflow.component.domain.DFFeatureRegistryUseCase
+import com.kuru.featureflow.component.register.DFFeatureConfig
+import com.kuru.featureflow.component.register.DFFeatureInterceptor
+import com.kuru.featureflow.component.register.DFRegistryComponentEntry
 import com.kuru.featureflow.component.register.DFRegistryEntryPoint
 import dagger.hilt.android.EntryPointAccessors
 
-class PlantEntry : DFComponentEntry {
+class PlantEntry : DFRegistryComponentEntry {
+
     companion object {
-        private const val TAG = "DynamicFeatureManager"
+        private const val TAG = "PlantEntry"
     }
 
     constructor() {
         // Public no-arg constructor for ServiceLoader
     }
+
     override fun initialize(context: Context) { // Method from DFComponentEntry interface
+        Log.d(TAG, "Get EntryPointAccessors from Context ")
         // 1. Get the Entry Point accessor
         val entryPoint = EntryPointAccessors.fromApplication(
             context.applicationContext,
             DFRegistryEntryPoint::class.java // Use the specific Entry Point interface
         )
 
+        Log.d(TAG, "EntryPointAccessors from Context is =  $entryPoint")
+        Log.d(TAG, "Get DFFeatureRegistryUseCase from ComponentRegistry ")
         // 2. Get the DFComponentRegistry via the Entry Point
-        val registry: DFComponentRegistry = entryPoint.getComponentRegistry()
-
+        val registry: DFFeatureRegistryUseCase = entryPoint.getComponentRegistry()
+        Log.d(TAG, "DFFeatureRegistryUseCase from ComponentRegistry  is $registry")
         // 3. Now use the registry
         val config = plantConfig() // Your feature-specific config
         registry.register(config) { navController, params ->
-            PlantsFeatureScreen(navController,params) // Your feature's screen
+            Log.d(TAG, "Navigating to PlantsFeatureScreen ")
+            PlantsFeatureScreen(navController, params) // Your feature's screen
         }
-        Log.d("TAG","Plant feature initialized and registered!")
+        Log.d(TAG, "Plant feature initialized and registered!")
     }
 
     // Helper function to create config
-    private fun plantConfig(): DFComponentConfig {
-        return DFComponentConfig(
+    private fun plantConfig(): DFFeatureConfig {
+        return DFFeatureConfig.create(
             route = "feature_plants",
-            listOfDFComponentInterceptor = listOf(
-                // Pre-install interceptor: Check network
-                DFComponentInterceptor(preInstall = true) {
-                    val isNetworkAvailable = true // Hypothetical function
-                    if (!isNetworkAvailable) println("No network for plants feature")
-                    isNetworkAvailable
-                },
-                // Post-install interceptor
-                DFComponentInterceptor(preInstall = false) {
-                    try {
-                        // Analytics
-                        println("Analytics for plants feature")
-                        true
-                    } catch (e: Exception) {
-                        println("Failed  $e")
-                        false
-                    }
+            // Post-install interceptor
+            DFFeatureInterceptor(preInstall = false) {
+                try {
+                    Log.d(TAG, "Analytics for plants feature")
+                    true
+                } catch (e: Exception) {
+                    Log.e(TAG, "Failed  $e")
+                    false
                 }
-            )
+            }
         )
     }
+
 }
