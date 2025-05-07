a20f1921833b25d402323cff8afe5afc13d478c5
Jerry Kuru <jerrykuru@gmail.com>
2025-05-07 12:16:12 -0700

    clean up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     namespace = "com.kuru.nextgen"
     compileSdk = 34 // Correct
 
-    val versionMajor = 87
+    val versionMajor = 90
     val versionMinor = 1
     val versionPatch = 0
 
